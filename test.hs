94210a05f71ec54b6d18e765483be5ad6ce00cf5
Olegs Bogdanovs <bogdanovsolegs@gmail.com>
2018-06-03 20:24:04 +0300

    sdfsdf

--- test.hs
+++ test.hs
@@ -1,4 +1,5 @@
-import qualified Data.PQueue as PQ
+import qualified Data.PQueue.Prio.Min as PQ
+import qualified Data.HashMap.Lazy as HM
 
 
 data Edge = Ed {
@@ -26,7 +27,7 @@ data Vertex' = Vtx {
 
 data Nodelist = Nls [Vertex]
 
-graph1 = (Nls [
+graph = (Nls [
                 (Vt "A" 0 0 [(Ed "B" 2), (Ed "C" 4)]),
                 (Vt "B" 1 1 [(Ed "A" 2), (Ed "C" 2), (Ed "D" 3)]),
                 (Vt "C" 2 2 [(Ed "A" 4), (Ed "B" 2), (Ed "D" 2)]),
@@ -35,8 +36,19 @@ graph1 = (Nls [
                 ])
 
 
-astar :: Nodelist -> String -> String -> Double -> [String]
-astar (Nls nodes) start _ _ | (null(filter (\x -> name x == start) nodes))  = ["Wrong start vertex!"]
-astar (Nls nodes) _ goal _ | (null(filter (\x -> name x == goal) nodes)) = ["Wrong goal vertex!"]
-astar (Nls node) start goal maxCost = ["Right"]
+astarSearch :: Nodelist -> String -> String -> Double -> [String]
+astarSearch (Nls nodes) start _ _ | (null(filter (\x -> name x == start) nodes))  = ["Wrong start vertex!"]
+astarSearch (Nls nodes) _ goal _ | (null(filter (\x -> name x == goal) nodes)) = ["Wrong goal vertex!"]
+astarSearch  (Nls node) start goal maxCost = ["Right"]
+
+astar :: PQ.MinPQueue dk vtv -> Nls ->  HM.Map sk sk-> HM.Map sk dk-> Vertex -> Vertex -> HM.Map sk sk
+astar frontier nodes came_from cost_so_far current goal | (name current) == (name goal = came_from
+astar frontier nodes came_from cost_so_far current goal | PQ.null frontier = came_from
+aster frontier nodes came_from cost_so_far current goal = if 
+							 
+
+
+heuristic :: Vertex -> Vertex -> Double
+heuristic (Vt _ x1 y1 _) (Vt _ x2 y2 _) = sqrt ((x1 - x2)^2 + (y1 - y2)^2)
+
 
